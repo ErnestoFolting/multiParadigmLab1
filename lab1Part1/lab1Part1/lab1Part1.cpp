8aad8adaf6b810f37d134e10ec4ea32187f45ab3
Danil <danilsmy.edu@gmail.com>
2022-02-05 21:47:17 +0200

    Add stopWords list

--- lab1Part1/lab1Part1/lab1Part1.cpp
+++ lab1Part1/lab1Part1/lab1Part1.cpp
@@ -1,6 +1,25 @@
 ﻿#include <iostream>
+#include <fstream>
 
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    string* stopWords = new string[15];
+    stopWords[0] = "in";
+    stopWords[1] = "on";
+    stopWords[2] = "up";
+    stopWords[3] = "an";
+    stopWords[4] = "a";
+    stopWords[5] = "are";
+    stopWords[6] = "is";
+    stopWords[7] = "has";
+    stopWords[8] = "so";
+    stopWords[9] = "be";
+    stopWords[10] = "were";
+    stopWords[11] = "do";
+    stopWords[12] = "did";
+    stopWords[13] = "can";
+    stopWords[14] = "for";
+    int i = 0;
+    int size = 15;
 }
